7b1e3dfe13ebda488b80471a5741ead92a35b4f7
Lauren Shindo <lauren.shindo@gmail.com>
2018-08-02 10:14:57 -0400

    Added first line

--- src/grandcircus/co/midterm/Story.java
+++ src/grandcircus/co/midterm/Story.java
@@ -3,8 +3,9 @@ package grandcircus.co.midterm;
 public class Story {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Once upon a time in the mystical land of "
+				+ "Shangrila there dwelt an immortal named Kai-Yun and his "
+				+ "trusty band of ninja monkeys.");	
 	}
 
 }
