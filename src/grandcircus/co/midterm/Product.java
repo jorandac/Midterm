b3e64a98028496e75d833c71120866886be33de6
Lauren Shindo <lauren.shindo@gmail.com>
2018-08-02 13:58:32 -0400

    added Product superclass

--- src/grandcircus/co/midterm/Product.java
+++ src/grandcircus/co/midterm/Product.java
@@ -2,4 +2,55 @@ package grandcircus.co.midterm;
 
 public class Product {
 
+		private String name;
+		private String description;
+		private double price;
+		
+		//constructor using fields
+		public Product(String name, String description, double price) {
+			super();
+			this.name = name;
+			this.description = description;
+			this.price = price;
+		}
+		
+		// empty constructor
+		public Product() {
+			
+		}
+
+		//getters and setters
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getDescription() {
+			return description;
+		}
+
+		public void setDescription(String description) {
+			this.description = description;
+		}
+
+		public double getPrice() {
+			return price;
+		}
+
+		public void setPrice(double price) {
+			this.price = price;
+		}
+
+		@Override
+		public String toString() {
+			return "Product [name=" + name + ", description=" + description + ", price=" + price + "]";
+		}
+		
+		
+		
+		
+		
 }
