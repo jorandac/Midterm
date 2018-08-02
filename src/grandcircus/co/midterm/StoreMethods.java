aaa47387050919d170903301debf9055bfabc0fc
cameronindetroit <cameronindetroit@gmail.com>
2018-08-02 15:22:08 -0400

    DisplayMenu Method update

--- src/grandcircus/co/midterm/StoreMethods.java
+++ src/grandcircus/co/midterm/StoreMethods.java
@@ -1,17 +1,33 @@
 package grandcircus.co.midterm;
 
+import java.util.List;
+
 public class StoreMethods {
 
-	//display menu
-	
-	//Choose item
-	
-	//Display total
-	
-	//Get payment
-	
-	//Display receipt
-	
-	//Store payment info to .txt file
-	
+	// display menu
+
+	public static void displayMenu(List<Product> products) {
+
+		int num = 1;
+
+		for (Product product : products) {
+
+			System.out.println(num + ". " + product);
+
+			num++;
+
+		}
+
+	}
+
+	// Choose item
+
+	// Display total
+
+	// Get payment
+
+	// Display receipt
+
+	// Store payment info to .txt file
+
 }
