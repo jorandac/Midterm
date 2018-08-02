0ad91080f54ac84424e226bf43720870a63e4d9e
cameronindetroit <cameronindetroit@gmail.com>
2018-08-02 10:44:27 -0400

    changed Monkeys to Students

--- src/grandcircus/co/midterm/Story.java
+++ src/grandcircus/co/midterm/Story.java
@@ -4,20 +4,22 @@ public class Story {
 
 	public static void main(String[] args) {
 		System.out.println("Once upon a time in the mystical land of "
-				+ "Shangrila there dwelt an immortal named Kai-Yun and his " + "trusty band of ninja monkeys.");
+				+ "Shangrila there dwelt an immortal named Kai-Yun and his " + "trusty band of students.");
 
 		System.out.println("Kai-Yun was very defient at all times, and lived by the beat by his own drum.");
 
-		System.out.println("His ninja monkeys would often bow down to him through out the day, which typically would "
+		System.out.println("His students would often bow down to him through out the day, which typically would "
 				+ "boost his ego");
 
-		System.out.println("Today, however, was not a typical day. When he emerged from his yurt, his trusty ninja"
-				+ " monkeys eyed him suspiciously.");
+		System.out.println("Today, however, was not a typical day. When he emerged from his yurt, his trusty stu"
+				+ " dents eyed him suspiciously.");
 
 		System.out.println("He thought to himself, Is he initiating a challenge? "
 				+ " and prepared his mind, body and soul for the battle to come");
-		
-		System.out.println("But it wasn't your typical battle, His ninja's challenged him to a dance battle"); 
+
+		System.out.println("But it wasn't your typical battle, His students challenged him to a dance battle");
+
+		System.out.println("Cams change ");
 	}
 
 }
