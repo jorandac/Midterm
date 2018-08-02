077b5cd0dd003edc3bd965dd7bb9604c74dd772b
Lauren Shindo <lauren.shindo@gmail.com>
2018-08-02 14:07:21 -0400

    added Payment superclass

--- src/grandcircus/co/midterm/Payment.java
+++ src/grandcircus/co/midterm/Payment.java
@@ -2,4 +2,35 @@ package grandcircus.co.midterm;
 
 public class Payment {
 
+		private String paymentType;
+		private double amount;
+		
+		//constructor using fields
+		public Payment(String paymentType, double amount) {
+			super();
+			this.paymentType = paymentType;
+			this.amount = amount;
+		}
+		
+		//empty constructor
+		public Payment() {
+			
+		}
+		
+		//getters and setters
+		public String getPaymentType() {
+			return paymentType;
+		}
+		public void setPaymentType(String paymentType) {
+			this.paymentType = paymentType;
+		}
+		public double getAmount() {
+			return amount;
+		}
+		public void setAmount(double amount) {
+			this.amount = amount;
+		}
+		
+		
+	
 }
