4b58948bc39756102e30d695f1f2b4015c7e1c67
JoRanda <jorandachapman@gmail.com>
2018-08-02 10:23:22 -0400

    JoRandas Commit

--- src/grandcircus/co/midterm/Story.java
+++ src/grandcircus/co/midterm/Story.java
@@ -7,6 +7,9 @@ public class Story {
 				+ "Shangrila there dwelt an immortal named Kai-Yun and his " + "trusty band of ninja monkeys.");
 
 		System.out.println("Kai-Yun was very defient at all times, and lived by the beat by his own drum.");
+		
+		System.out.println("His ninja monkeys would often bow down to him through out the day, which typically would "
+				+ "boost his ego");  
 	}
 
 }
