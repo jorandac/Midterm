4378597edbd4ef1f13e22cab13aca573d35979ac
Lauren Shindo <lauren.shindo@gmail.com>
2018-08-02 13:20:32 -0400

    updated methods with comments

--- src/grandcircus/co/midterm/StoreMethods.java
+++ src/grandcircus/co/midterm/StoreMethods.java
@@ -2,4 +2,18 @@ package grandcircus.co.midterm;
 
 public class StoreMethods {
 
+	//display menu
+	
+	//Choose item
+	
+	//Display total
+	
+	//Get payment
+	
+	//Display receipt
+	
+	//Store payment info to .txt file
+	
+	
+	
 }
