3cafa15e99bf39d939c167c7d09c1911c3f0c868
Lauren Shindo <lauren.shindo@gmail.com>
2018-08-02 14:13:49 -0400

    added check subclass

--- src/grandcircus/co/midterm/Check.java
+++ src/grandcircus/co/midterm/Check.java
@@ -2,4 +2,30 @@ package grandcircus.co.midterm;
 
 public class Check extends Payment {
 
+	private int checkNum;
+
+	//constructor
+	public Check(String paymentType, double amount, int checkNum) {
+		super(paymentType, amount);
+		this.checkNum = checkNum;
+	}
+	
+	//empty constructor
+	public Check() {
+		
+	}
+
+	// getter
+	public int getCheckNum() {
+		return checkNum;
+	}
+
+	//setter
+	public void setCheckNum(int checkNum) {
+		this.checkNum = checkNum;
+	}
+	
+	
+	
+	
 }
