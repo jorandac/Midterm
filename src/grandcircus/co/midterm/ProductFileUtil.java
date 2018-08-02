ef2ddbd66aa9c003519a00424b9d698a08f5a65a
cameronindetroit <cameronindetroit@gmail.com>
2018-08-02 14:47:29 -0400

    ProductFileUtil update

--- src/grandcircus/co/midterm/ProductFileUtil.java
+++ src/grandcircus/co/midterm/ProductFileUtil.java
@@ -2,4 +2,17 @@ package grandcircus.co.midterm;
 
 public class ProductFileUtil {
 
+	// The path to the file to use
+	public static final String FILE_NAME = "Menu.txt";
+
+	// Modify this method as necessary to convert a line of text from the file to a
+	// new item instance
+	private static Product convertLineToItem(String line) {
+		String[] products = line.split("\t");
+		Product product = new Product();
+		product.setName(products[0]);
+		product.setDescription(products[1]);
+		product.setPrice(Double.parseDouble(products[2]));
+		return product;
+	}
 }
