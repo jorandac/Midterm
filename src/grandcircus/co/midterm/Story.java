ad25b9ce7a9c26ee586f11a68532e9e064839c87
cameronindetroit <cameronindetroit@gmail.com>
2018-08-02 10:18:42 -0400

    cams commit

--- src/grandcircus/co/midterm/Story.java
+++ src/grandcircus/co/midterm/Story.java
@@ -4,8 +4,9 @@ public class Story {
 
 	public static void main(String[] args) {
 		System.out.println("Once upon a time in the mystical land of "
-				+ "Shangrila there dwelt an immortal named Kai-Yun and his "
-				+ "trusty band of ninja monkeys.");	
+				+ "Shangrila there dwelt an immortal named Kai-Yun and his " + "trusty band of ninja monkeys.");
+
+		System.out.println("Kai-Yun was very defient at all times, and lived by the beat by his own drum.");
 	}
 
 }
