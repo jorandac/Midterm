61a1e062cc4d8533c9f42d0f69e8cb3d85591a91
cameronindetroit <cameronindetroit@gmail.com>
2018-08-02 10:31:33 -0400

    cams second commit

--- src/grandcircus/co/midterm/Story.java
+++ src/grandcircus/co/midterm/Story.java
@@ -7,12 +7,15 @@ public class Story {
 				+ "Shangrila there dwelt an immortal named Kai-Yun and his " + "trusty band of ninja monkeys.");
 
 		System.out.println("Kai-Yun was very defient at all times, and lived by the beat by his own drum.");
-		
+
 		System.out.println("His ninja monkeys would often bow down to him through out the day, which typically would "
-				+ "boost his ego");  
-		
+				+ "boost his ego");
+
 		System.out.println("Today, however, was not a typical day. When he emerged from his yurt, his trusty ninja"
 				+ " monkeys eyed him suspiciously.");
+
+		System.out.println("He thought to himself, Is he initiating a challenge? "
+				+ " and prepared his mind, body and soul for the battle to come");
 	}
 
 }
