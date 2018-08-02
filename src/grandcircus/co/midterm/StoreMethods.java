32fb225bf154cede3e811c722fda6334b65e0f31
cameronindetroit <cameronindetroit@gmail.com>
2018-08-02 15:54:50 -0400

    Display Total update

--- src/grandcircus/co/midterm/StoreMethods.java
+++ src/grandcircus/co/midterm/StoreMethods.java
@@ -23,6 +23,25 @@ public class StoreMethods {
 	// Choose item
 
 	// Display total
+	public static void displayTotal(List<Product> shoppingCart) {
+		int num = 1;
+		double total = 0;
+		double salesTax = 0.06;
+
+		for (Product product : shoppingCart) {
+
+			System.out.println(num + ". " + product);
+
+			num++;
+
+			total += product.getPrice();
+		}
+
+		System.out.println("Your Subtotal is " + total);
+
+		System.out.println("Your Total with tax is " + total * salesTax);
+
+	}
 
 	// Get payment
 
