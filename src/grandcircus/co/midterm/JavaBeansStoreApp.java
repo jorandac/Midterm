0d0fdb19e35e0c6f9d19af782f7e930ed98d4d22
cameronindetroit <cameronindetroit@gmail.com>
2018-08-03 12:58:39 -0400

    Category added to Product Class

--- src/grandcircus/co/midterm/JavaBeansStoreApp.java
+++ src/grandcircus/co/midterm/JavaBeansStoreApp.java
@@ -14,58 +14,58 @@ public class JavaBeansStoreApp {
 		List<Product> menu = ProductFileUtil.readFile();
 		// Shopping cart
 		List<Product> shoppingCart = new ArrayList<>();
-		String userContinues = "n"; //keeps track of whether user wants to continue in the loop
 
-		//Welcome message to user
+		String userContinues = "n"; // keeps track of whether user wants to continue in the loop
+
+		// Welcome message to user
 		System.out.println("Welcome to Java Beans!\n");
-		
-		
+
 		// Display Menu
 		StoreMethods.displayMenu(menu);
 
-		//loop starts here
+		// loop starts here
 		do {
-		
-		//TODO: Ask user to choose item
-			int productChoice = Validator.getInt(scnr, "\nPlease choose an item from our menu "
-					+ "(enter a number): ", 1, menu.size());
-			scnr.nextLine(); //clear scanner for taking in string next
-		
-			//TODO: if user chooses drink, ask for hot or cold choice
-			//TODO: if user chooses sandwich, ask for options:
-				//TODO: meat, cheese, egg/no egg, bread
+
+			// TODO: Ask user to choose item
+			int productChoice = Validator.getInt(scnr, "\nPlease choose an item from our menu " + "(enter a number): ",
+					1, menu.size());
+			scnr.nextLine(); // clear scanner for taking in string next
+
+			// TODO: if user chooses drink, ask for hot or cold choice
+			// TODO: if user chooses sandwich, ask for options:
+			// TODO: meat, cheese, egg/no egg, bread
 			if (menu.get(productChoice).getName().contains("Breakfast Sandwich")) {
-				
+
 				String sandwichBread = Validator.getString(scnr, "\nWhat type of bread would you like? ");
 			}
-			//TODO: store user choice in ArrayList
-		
-		//Ask if user wants to add more items , loop ends here
+			// TODO: store user choice in ArrayList
+
+			// Ask if user wants to add more items , loop ends here
 			userContinues = Validator.getString(scnr, "\nWould you like to add anything else to your order? (y/n) ");
-		
+
 		} while (userContinues.matches("[yY].*"));
-		
-		//TODO: display subtotal
-		
-		//TODO: ask for payment type Switch/Case
-		int paymentChoice = Validator.getInt(scnr, "\nHow would you like to pay? "
-				+ "(Choose 1 for cash, 2 for credit, or 3 for check): ", 1, 3);
+
+		// TODO: display subtotal
+
+		// TODO: ask for payment type Switch/Case
+		int paymentChoice = Validator.getInt(scnr,
+				"\nHow would you like to pay? " + "(Choose 1 for cash, 2 for credit, or 3 for check): ", 1, 3);
 		System.out.println(); // blank line for readability
-		
-			switch (paymentChoice) {
-				case 1:
-					//cashMethod
-					break;
-				case 2:
-					//creditMethod, write to file
-					break;
-				case 3:
-					//check method, write to file
-					break;
-					
-			}
-		
-		//TODO: display receipt
+
+		switch (paymentChoice) {
+		case 1:
+			// cashMethod
+			break;
+		case 2:
+			// creditMethod, write to file
+			break;
+		case 3:
+			// check method, write to file
+			break;
+
+		}
+
+		// TODO: display receipt
 	}
 
 }
--- src/grandcircus/co/midterm/Product.java
+++ src/grandcircus/co/midterm/Product.java
@@ -2,55 +2,61 @@ package grandcircus.co.midterm;
 
 public class Product {
 
-		private String name;
-		private String description;
-		private double price;
-		
-		//constructor using fields
-		public Product(String name, String description, double price) {
-			super();
-			this.name = name;
-			this.description = description;
-			this.price = price;
-		}
-		
-		// empty constructor
-		public Product() {
-			
-		}
+	private String name;
+	private String description;
+	private double price;
+	private String category;
 
-		//getters and setters
-		public String getName() {
-			return name;
-		}
+	// constructor using fields
+	public Product(String name, String description, double price, String category) {
+		super();
+		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.category = category;
+	}
 
-		public void setName(String name) {
-			this.name = name;
-		}
+	// empty constructor
+	public Product() {
 
-		public String getDescription() {
-			return description;
-		}
+	}
 
-		public void setDescription(String description) {
-			this.description = description;
-		}
+	// getters and setters
+	public String getName() {
+		return name;
+	}
 
-		public double getPrice() {
-			return price;
-		}
+	public void setName(String name) {
+		this.name = name;
+	}
 
-		public void setPrice(double price) {
-			this.price = price;
-		}
+	public String getcategory() {
+		return category;
+	}
 
-		@Override
-		public String toString() {
-			return "Product [name=" + name + ", description=" + description + ", price=" + price + "]";
-		}
-		
-		
-		
-		
-		
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Product [name=" + name + ", description=" + description + ", price=" + price + "]";
+	}
+
 }
