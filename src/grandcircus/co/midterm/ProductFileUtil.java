28be498e713fa64a7cc469312cbd02523db36e3d
cameronindetroit <cameronindetroit@gmail.com>
2018-08-06 13:43:46 -0400

    writeFile method update

--- src/grandcircus/co/midterm/ProductFileUtil.java
+++ src/grandcircus/co/midterm/ProductFileUtil.java
@@ -22,21 +22,21 @@ public class ProductFileUtil {
 		if (products[1].equals("drink")) {
 			Drink drink = new Drink(products[0], products[2], Double.parseDouble(products[3]), products[1], "", "");
 			return drink;
-		}
-		else if (products[0].contains("sandwich")) {
-			Sandwich sandwich = new Sandwich(products[0], products[2], Double.parseDouble(products[3]), products[1], "", "", "", "");
+		} else if (products[0].contains("sandwich")) {
+			Sandwich sandwich = new Sandwich(products[0], products[2], Double.parseDouble(products[3]), products[1], "",
+					"", "", "");
 			return sandwich;
 		}
-			
+
 		else {
-		Product product = new Product();
-		product.setName(products[0]);
-		product.setDescription(products[2]);
-		product.setPrice(Double.parseDouble(products[3]));
-		product.setCategory(products[1]);
-		return product;
+			Product product = new Product();
+			product.setName(products[0]);
+			product.setDescription(products[2]);
+			product.setPrice(Double.parseDouble(products[3]));
+			product.setCategory(products[1]);
+			return product;
 		}
-		
+
 //		Product product = new Product();
 //		product.setName(products[0]);
 //		product.setDescription(products[2]);
@@ -82,18 +82,16 @@ public class ProductFileUtil {
 	}
 
 	// Write to file
-	public static void writeFile(List<Product> items) {
+	public static void writeFile(String customerInformation) {
 		try (
 				// The `false` here tells the FileOutputStream to overwrite the file, rather
 				// than append to it
-				FileOutputStream fileOutputStream = new FileOutputStream(FILE_NAME2, false);
+				FileOutputStream fileOutputStream = new FileOutputStream(FILE_NAME2, true);
 				PrintWriter fileWriter = new PrintWriter(fileOutputStream);) {
 			// write to the file
-			for (Product item : items) {
-				// each item must be converted to a string of text to write to the file
-				String line = convertItemToLine(item);
-				fileWriter.println(line);
-			}
+
+			String line = customerInformation;
+			fileWriter.println(line);
 
 		} catch (IOException e) {
 			// If something else crazy goes wrong, print out the error.
@@ -101,5 +99,4 @@ public class ProductFileUtil {
 			e.printStackTrace();
 		}
 	}
-
 }
